50e2e72b3e37e571e88c051ba070e01efe4bf0a8
edirin-nelson <nelsonedirin@gmail.com>
2023-03-22 15:00:37 +0100

    Implemented AddToCart servlet

--- src/main/java/com/marketplace/servlet/QuantityIncDecServlet.java
+++ src/main/java/com/marketplace/servlet/QuantityIncDecServlet.java
@@ -11,9 +11,12 @@ import java.util.ArrayList;
 
 @WebServlet(name = "QuantityIncDecServlet", value = "/quantity-inc-dec")
 public class QuantityIncDecServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTH-8");
+
+        response.setContentType("text/html;charset=UTF-8");
         try(PrintWriter out = response.getWriter();) {
             String action = request.getParameter("action");
             int id = Integer.parseInt(request.getParameter("id"));
